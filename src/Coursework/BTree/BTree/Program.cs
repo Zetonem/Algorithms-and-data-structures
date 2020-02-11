9dc217557d858954897703e95258a6ffdbacab7a
lz5 <leonid.zaytsev.lz5@gmail.com>
2020-02-11 16:10:40 +0300

    Add coursework project.

---
+++ src/Coursework/BTree/BTree/Program.cs
@@ -0,0 +1,29 @@
+﻿/* FILE NAME   : Program.cs
+ * PROGRAMMER  : Leonid Zaytsev.
+ * LAST UPDATE : 11.02.2020.
+ * NOTE        : None.
+ *
+ * Copyright © 2020 Leonid Zaytsev. All rights reserved.
+ */
+using System;
+
+// Project namespace
+namespace BTree
+{
+    /// <summary>
+    /// Сlass containing entry point to the program.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The main program method.
+        /// </summary>
+        /// <param name="args">Input parameters to the program</param>
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        } // End of 'Main' method
+    } // End of 'Program' class
+} // end of 'BTree' namespace
+
+// END OF 'Program.cs' FILE
