5ee7573bcc543817aedbf504d3fbbf37d995b883
Leonid Zaytsev <Leonid.Zaytsev.lz5@gmail.com>
2020-05-04 17:18:53 +0300

    Add main functionality of the B-Tree.

--- src/Coursework/BTree/BTree/Node.cs
+++ src/Coursework/BTree/BTree/Node.cs
@@ -1,6 +1,6 @@
 ﻿/* FILE NAME   : Node.cs
  * PROGRAMMER  : Leonid Zaytsev.
- * LAST UPDATE : 03.05.2020.
+ * LAST UPDATE : 04.05.2020.
  * NOTE        : None.
  *
  * Copyright © 2020 Leonid Zaytsev. All rights reserved.
@@ -15,12 +15,18 @@ namespace BTree
     /// Defines node of the B-Tree data structure.
     /// </summary>
     /// <typeparam name="Type">The type of object in the node.</typeparam>
+    [Serializable]
     public sealed class Node<Type> where Type : IComparable<Type>
     {
         /// <summary>
         /// List of keys in the node.
         /// </summary>
         public List<Type> Keys { get; } = new List<Type>();
+
+        /// <summary>
+        /// Reference to the parent node.
+        /// </summary>
+        public Node<Type> Parent { get; set; }
 
         /// <summary>
         /// List of children nodes.
@@ -31,6 +37,15 @@ namespace BTree
         /// Check whether it is a leaf or not.
         /// </summary>
         public Boolean IsLeaf => Children.Count == 0;
+
+        /// <summary>
+        /// Class constructor by reference to the parent node.
+        /// </summary>
+        /// <param name="parent"></param>
+        public Node(Node<Type> parent = null)
+        {
+            Parent = parent;
+        } // End of 'Node' constructor
     } // End of 'Node' class
 } // end of 'BTree' namespace
 
